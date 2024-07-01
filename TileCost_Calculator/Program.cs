a2a46ab1901a2815f589f9628aafdc10284bee8f
Sosa09 <soufiane.arrazouki@hotmail.com>
2024-07-01 23:13:12 +0200

    Easy Task finsihed

--- TileCost_Calculator/Program.cs
+++ TileCost_Calculator/Program.cs
@@ -5,13 +5,23 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, welcomd to tile cost calculator: \nlet's start !");
-            float tileWidth;
-            float tileHeight;
-  
+            
+            Console.WriteLine("Enter the width: ");
+            float width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the height: ");
+            float height = Convert.ToInt32(Console.ReadLine()); ;
+            Console.WriteLine("Enter the cost per one unit of flooring");
+            float costPerOneUnit = Convert.ToInt32(Console.ReadLine()); ;
+
+            float area = width * height;
+            float totalCostFlooring = area * costPerOneUnit;
+
+            //calculate the cost to cover the area
+            Console.WriteLine($"The total cost for flooring an area of {area} is {totalCostFlooring}");
 
 
         }
 
-        private static void 
+
     }
 }

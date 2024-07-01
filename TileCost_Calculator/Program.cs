7e50bbc96d0f98a28421e9a4c9bf1fa77a45bac2
Sosa09 <soufiane.arrazouki@hotmail.com>
2024-07-01 23:02:49 +0200

    Add project files.

---
+++ TileCost_Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TileCost_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcomd to tile cost calculator: \nlet's start !");
+            float tileWidth;
+            float tileHeight;
+  
+
+
+        }
+
+        private static void 
+    }
+}

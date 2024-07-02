7acd7e854803b7aaf81dd4677b486b816107900f
Sosa09 <soufiane.arrazouki@hotmail.com>
2024-07-02 13:21:57 +0200

    Refactored from functions to Rekete mentoring based learning

--- TileCost_Calculator/Program.cs
+++ TileCost_Calculator/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace TileCost_Calculator
+﻿using System.Formats.Asn1;
+
+namespace TileCost_Calculator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            float totalCostFlooring = 0;
+            float area = 0;
+            string shape = "";
             Console.WriteLine("Hello, welcome to tile cost calculator: \nlet's start !");
             while (true)
             {
@@ -16,81 +21,67 @@
                 float costPerUnit = Convert.ToInt32(Console.ReadLine()); ;
 
                 if (choice == '1')
-                    _costForRectange(costPerUnit);
+                {
+                    // A = w * l
+                    Console.WriteLine("Enter the width: ");
+                    float width = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the height: ");
+                    float height = Convert.ToInt32(Console.ReadLine()); ;
+
+
+                    area = width * height;
+                    totalCostFlooring = area * costPerUnit;
+                    shape = "rectangle";
+
+                }
                 else if (choice == '2')
-                    _costForCircle(costPerUnit);
+                {
+                    //A = pi * R2
+                 
+                    double radius = Convert.ToInt32(Console.ReadLine());
+                    area = (float)(3.14 * Math.Pow(radius, 2.0)); //casting double to float
+
+
+                    totalCostFlooring = area * costPerUnit;
+                    shape = "Circle";
+                }
                 else if (choice == '3')
-                    _costForPolygon(costPerUnit);
+                {
+                    //A = 1/2 * p * apothem
+                    Console.WriteLine("Enter the perimeter: ");
+                    float perimeter = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the apothem: ");
+                    float apothem = Convert.ToInt32(Console.ReadLine()); ;
+
+
+                    area = 1 / 2 * perimeter * apothem;
+                    totalCostFlooring = area * costPerUnit;
+                    shape = "Polygon";
+                }
                 else if (choice == '4')
-                    _costForTriangle(costPerUnit);
+                {
+                    //A = Hb * b / 2
+                    Console.WriteLine("Enter the base: ");
+                    float baseOfATriangle = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the height: ");
+                    float heightOfATriangle = Convert.ToInt32(Console.ReadLine()); ;
+
+
+                    area = heightOfATriangle * baseOfATriangle / 2;
+                    totalCostFlooring = area * costPerUnit;
+                    shape = "Triangle";
+                }
                 else
                 {
                     //ADDED Difficulty notify the user that the given shape is not supported
                     Console.WriteLine("Shape not supported try again !");
+                    break;
                 }
+
+                Console.WriteLine($"The total cost for flooring an {shape} shaped area of {area} is {totalCostFlooring}");
             }
 
         }
-        private static void _costForRectange(float costPerUnit) 
-        {
-            // A = w * l
-            Console.WriteLine("Enter the width: ");
-            float width = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the height: ");
-            float height = Convert.ToInt32(Console.ReadLine()); ;
- 
 
-            float area = width * height;
-            float totalCostFlooring = area * costPerUnit;
-
-            //calculate the cost to cover the area
-            Console.WriteLine($"The total cost for flooring an rectangle shaped area of {area} is {totalCostFlooring}");
-        }
-        private static void _costForCircle(float costPerUnit)
-        {
-            //A = pi * R2
-            //using double since math.pow canonly be used using double
-            double radius = Convert.ToInt32(Console.ReadLine());
-            double area = 3.14 * Math.Pow(radius, 2.0);
-            
-
-            double totalCostFlooring = area * (double)costPerUnit;
-
-            //calculate the cost to cover the area
-            Console.WriteLine($"The total cost for flooring a circle shaped area of {area} is {totalCostFlooring}");
-        }
-
-        private static void _costForPolygon(float costPerUnit)
-        {
-            //A = 1/2 * p * apothem
-            Console.WriteLine("Enter the perimeter: ");
-            float perimeter = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the apothem: ");
-            float apothem = Convert.ToInt32(Console.ReadLine()); ;
-
-
-            float area = 1/2 * perimeter * apothem;
-            float totalCostFlooring = area * costPerUnit;
-
-            //calculate the cost to cover the area
-            Console.WriteLine($"The total cost for flooring an polygon shaped area of {area} is {totalCostFlooring}");
-
-        }
-        private static void _costForTriangle(float costPerUnit)
-        {
-            //A = Hb * b / 2
-            Console.WriteLine("Enter the base: ");
-            float baseOfATriangle = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the height: ");
-            float heightOfATriangle = Convert.ToInt32(Console.ReadLine()); ;
-
-
-            float area = heightOfATriangle * baseOfATriangle / 2;
-            float totalCostFlooring = area * costPerUnit;
-
-            //calculate the cost to cover the area
-            Console.WriteLine($"The total cost for flooring an Triangle shaped area of {area} is {totalCostFlooring}");
-
-        }
     }
 }
